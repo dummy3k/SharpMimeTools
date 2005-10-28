adb5273e8efa65ad5a4107ea01aa8f4784718520
anmar <anmar@4b8e5d3a-8b10-0410-9ae9-e01485a2957d>
2005-10-28 20:15:57 +0000

    - [SharpMimeHeader] MimeTopLevelMediaType values didn't allow bitwise operations.
    git-svn-id: https://sharpwebmail.svn.sourceforge.net/svnroot/sharpwebmail/trunk/SharpMimeTools@195 4b8e5d3a-8b10-0410-9ae9-e01485a2957d

--- src/SharpMimeHeader.cs
+++ src/SharpMimeHeader.cs
@@ -424,35 +424,35 @@ namespace anmar.SharpMimeTools
 	/// <summary>
 	/// RFC 2046 Initial top-level media types
 	/// </summary>
-	[System.Flags]
+	[Flags]
 	public enum MimeTopLevelMediaType {
 		/// <summary>
 		/// RFC 2046 section 4.1
 		/// </summary>
-		text,
+		text = 0,
 		/// <summary>
 		/// RFC 2046 section 4.2
 		/// </summary>
-		image,
+		image = 1,
 		/// <summary>
 		/// RFC 2046 section 4.3
 		/// </summary>
-		audio,
+		audio = 2,
 		/// <summary>
 		/// RFC 2046 section 4.4
 		/// </summary>
-		video,
+		video = 4,
 		/// <summary>
 		/// RFC 2046 section 4.5
 		/// </summary>
-		application,
+		application = 8,
 		/// <summary>
 		/// RFC 2046 section 5.1
 		/// </summary>
-		multipart,
+		multipart = 16,
 		/// <summary>
 		/// RFC 2046 section 5.2
 		/// </summary>
-		message
+		message = 32
 	}
 }
